0299e9afdbef337e38a14cd7dc6a2be6dd5463aa
guankai <Rootroot1>
2017-05-27 11:22:29 +0800

    添加基础信息管理的modal

--- src/app/components/monitor/basicmanage.controller.js
+++ src/app/components/monitor/basicmanage.controller.js
@@ -10,6 +10,14 @@
     function basicManageController(constdata, NetworkService, MapService, $stateParams, ApiServer, toastr, $state, $timeout, $interval,$scope) {
         /* jshint validthis: true */
         var vm = this;
+        $scope.modalInput = false;
+        $scope.toggleModal = function() {
+            $scope.modalInput = !$scope.modalInput;
+        };
+        $scope.modalUpdate = false;
+        $scope.toggleUpdate = function(){
+            $scope.modalUpdate = !$scope.modalUpdate;
+        }
     }
 
 
