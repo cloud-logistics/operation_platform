fc01ee0e2f5375fffcc031b21466b4b46518d230
guankai <Rootroot1>
2017-06-07 17:36:26 +0800

    提交机器人相关

---
+++ src/app/components/monitor/robots.controller.js
@@ -0,0 +1,19 @@
+/**
+ * Created by guankai on 07/06/2017.
+ */
+(function () {
+    'use strict';
+
+    angular.module('smart_container').controller('RobotsController', RobotsController);
+
+    /** @ngInject */
+    function RobotsController(constdata, NetworkService, $stateParams, ApiServer, toastr, $state, $timeout, $interval,$scope, optionsTransFunc) {
+        /* jshint validthis: true */
+        var vm = this;
+
+
+
+
+    }
+
+})();
