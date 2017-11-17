e007738be2c1db67dae2190141b301639dd2d1e0
Wang Yi <linucywang@aliyun.com>
2017-11-17 16:59:15 +0800

    change heat map for prediction

--- src/app/components/predictionDecision/predictionDecision.controller.js
+++ src/app/components/predictionDecision/predictionDecision.controller.js
@@ -17,7 +17,7 @@
         var mapCenter = {lat: 31.2891, lng: 121.4648};
         vm.mapSize = {"width": width + 'px', "height": height + 'px'};
 
-        var map = MapService.map_init("map-canvas", mapCenter, "terrain");
+        var map = MapService.map_init("prediction-map", mapCenter, "terrain");
 
         $scope.conf = {
             currentPage: 1,
@@ -34,53 +34,33 @@
         // 鼠标绘图工具
         var overlay = undefined;
 
-        drawingManagerInit(map)
-
-        var heatmap = new HeatmapOverlay(map,
-            {
-                // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-                "radius": 2,
-                "maxOpacity": 1,
-                // scales the radius based on map zoom
-                "scaleRadius": true,
-                // if set to false the heatmap uses the global maximum for colorization
-                // if activated: uses the data maximum within the current map boundaries
-                //   (there will always be a red spot with useLocalExtremas true)
-                "useLocalExtrema": true,
-                // which field name in your data represents the latitude - default "lat"
-                latField: 'lat',
-                // which field name in your data represents the longitude - default "lng"
-                lngField: 'lng',
-                // which field name in your data represents the data value - default "value"
-                valueField: 'count'
-            }
-        );
+        var heatmap = undefined;
 
         var setHeatmap = function(){
             ApiServer.getDistribution({
                 data:"",
                 success:function(res){
-                    console.log("res = ",res.data);
-                    var max = 0;
-                    var data = _.map(res.data.sites,function(item){
-                        max = item.box_num < max ? max : item.box_num;
-                        return{
-                           "lng":item.longitude,
-                            "lat":item.latitude,
-                            count:item.box_num
-                        }
-                    });
-                    heatmap.setData({
-                        max:max,
-                        data:data
-                    });
+                    var bounds = new google.maps.LatLngBounds();
+
+                    var histData = R.map(function(item){
+                        var lng = parseFloat(item.longitude);
+                        var lat = parseFloat(item.latitude);
+                        var res = {
+                            location: new google.maps.LatLng(lat, lng),
+                            weight: item.box_num
+                        };
+                        return res;
+                    })(res.data.sites);
+                    console.log(histData);
+
                     heatmap = new google.maps.visualization.HeatmapLayer({
-                        data: histData,
-                        map: map
+                      data: histData,
+                      radius: 20,
+                      map: map
                     });
 
                     histData.map(function (item) {
-                        bounds.extend(item);
+                      bounds.extend(item.location);
                     })
 
                     map.fitBounds(bounds);
@@ -92,54 +72,6 @@
         };
         setHeatmap();
 
-        function drawingManagerInit(map) {
-            var styleOptions = {
-                // fillColor: '#ffff00',
-                fillColor:"red",      //填充颜色。当参数为空时，圆形将没有填充效果。
-                fillOpacity: 0.6,
-                strokeWeight: 3,
-                clickable: false,
-                editable: true,
-                strokeColor: "red",    //边线颜色。
-                strokeOpacity: 0.8,	   //边线透明度，取值范围0 - 1。
-                strokeStyle: 'solid', //边线的样式，solid或dashed。
-                zIndex: 1
-            }
-
-            var drawingManager = new google.maps.drawing.DrawingManager({
-                drawingMode: google.maps.drawing.OverlayType.null,
-                drawingControl: true,
-                drawingControlOptions: {
-                    position: google.maps.ControlPosition.TOP_CENTER,
-                    drawingModes: ['marker', 'circle', /*'polygon', 'polyline',*/ 'rectangle']
-                },
-                circleOptions: styleOptions,
-                rectangleOptions: styleOptions
-            });
-            drawingManager.setMap(map);
-
-            google.maps.event.addListener(drawingManager, 'circlecomplete', function (circle) {
-               var bounds = circle.getBounds();
-
-                updateMarker(bounds);
-            });
-
-            google.maps.event.addListener(drawingManager, 'rectanglecomplete', function (rectangle) {
-                var bounds = rectangle.getBounds();
-                updateMarker(bounds)
-            });
-
-            google.maps.event.addListener(drawingManager, 'overlaycomplete', function (event) {
-                overlay = event.overlay;
-
-                if (!R.isNil(overlay)) {
-                    setTimeout(function () {
-                        overlay.setMap(null)
-                    }, 500)
-                }
-
-            });
-        }
 
         $scope.updateMarker = function(bounds) {
             console.log("bounds",bounds);
