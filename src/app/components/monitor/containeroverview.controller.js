ae297afb50709b97c53b779c14efa1d4ee413468
xianZJ <13636810709@163.com>
2018-01-08 17:56:18 +0800

    云箱概览 重现bug 9044

--- src/app/components/monitor/containeroverview.controller.js
+++ src/app/components/monitor/containeroverview.controller.js
@@ -85,12 +85,13 @@
               console.log("Get getOperationOverview  Info Failed", err);
           });
         }
-        $scope.$on("mapResize_from_main_to_children",function(){
-            console.log("mapResize in children",map);
-            setTimeout(function(){
-                google.maps.event.trigger(map, 'resize')
-            },100);
-        })
+
+        //$scope.$on("mapResize_from_main_to_children",function(){
+        //    console.log("mapResize in children",map);
+        //    setTimeout(function(){
+        //        google.maps.event.trigger(map, 'resize')
+        //    },100);
+        //})
     }
 
 })();
