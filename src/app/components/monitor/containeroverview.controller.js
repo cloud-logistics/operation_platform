2a26df23fb4994b9dfaf4b7dfc85793c434c7541
xianZJ <13636810709@163.com>
2018-02-06 14:16:27 +0800

    modify 仓库预览

--- src/app/components/monitor/containeroverview.controller.js
+++ src/app/components/monitor/containeroverview.controller.js
@@ -7,7 +7,7 @@
     angular.module('smart_container').controller('ContainerOverviewController', ContainerOverviewController);
 
     /** @ngInject */
-    function ContainerOverviewController($stateParams, constdata, ApiServer, MapService, toastr,$state,$timeout,$interval,$scope) {
+    function ContainerOverviewController($stateParams, constdata, ApiServer, MapService, toastr, $state, $timeout, $interval, $scope) {
         /* jshint validthis: true */
         var vm = this;
 
@@ -15,7 +15,7 @@
         var height = document.body.clientHeight;
         var histories = [];
         var routes = [];
-        vm.mapSize = {"width":width + 'px',"height":height + 'px'};
+        vm.mapSize = {"width": width + 'px', "height": height + 'px'};
         var mapCenter = {lat: 31.2891, lng: 121.4648};
 
         var map = MapService.map_init("containeroverview", mapCenter, "terrain", 4);
@@ -29,8 +29,8 @@
             end_time: moment(new Date())
         };
 
-        $scope.validationCheck = function(){
-            $scope.isContainerIdInvalida = vm.queryParams.containerId != "" &&!constdata['validation']['id'].test(vm.queryParams.containerId);
+        $scope.validationCheck = function () {
+            $scope.isContainerIdInvalida = vm.queryParams.containerId != "" && !constdata['validation']['id'].test(vm.queryParams.containerId);
         };
 
         // 鼠标绘图工具
@@ -41,56 +41,54 @@
 
         function getHistorylocationInfo() {
             ApiServer.getContainerOverviewInfo(function (response) {
-                var containers = R.compose(
-                    R.map(function(item){
-                        console.log(item);
-                        return new google.maps.LatLng(item.lat, item.lng);
-                    }),
-                    R.filter(
-                      R.compose(
-                        R.not,
-                        R.equals({lng:0, lat:0})
-                      )
-                    ),
-                    R.map(R.prop("position")),
-                    R.prop("data")
-                )(response)
+                console.log("res = ", response);
+                var histData = R.map(function (item) {
+                    var lng = parseFloat(item.lng);
+                    var lat = parseFloat(item.lat);
+
+                    var res = {
+                        location: new google.maps.LatLng(lat, lng),
+                        weight: item.cnt
+                    };
+                    return res;
+                })(response.data);
 
                 var bounds = new google.maps.LatLngBounds();
 
-                if(R.isEmpty(containers)){
-                  toastr.error('未查询到云箱信息，请联系管理员！');
-                }else {
+                if (R.isEmpty(histData)) {
+                    toastr.error('未查询到云箱信息，请联系管理员！');
+                } else {
                     heatmap = new google.maps.visualization.HeatmapLayer({
-                      data: containers,
-                      map: map
+                        data: histData,
+                        map: map,
+                        radius: 20
                     });
 
-                    containers.map(function (item) {
-                      bounds.extend(item);
+                    histData.map(function (item) {
+                        bounds.extend(item.location);
                     })
 
                     map.fitBounds(bounds);
                 }
 
-            },function (err) {
+            }, function (err) {
                 console.log("Get Historyview Info Failed", err);
             });
         }
 
-        function getOperationOverview(){
-          ApiServer.getOperationOverview(function (response) {
-              $scope.container_num = response.data.container_num;
-          }, function (err) {
-              console.log("Get getOperationOverview  Info Failed", err);
-          });
+        function getOperationOverview() {
+            ApiServer.getOperationOverview(function (response) {
+                $scope.container_num = response.data.container_num;
+            }, function (err) {
+                console.log("Get getOperationOverview  Info Failed", err);
+            });
         }
 
-        $scope.$on("mapResize_from_main_to_children",function(){
-            console.log("mapResize in children",map);
-            setTimeout(function(){
+        $scope.$on("mapResize_from_main_to_children", function () {
+            console.log("mapResize in children", map);
+            setTimeout(function () {
                 google.maps.event.trigger(map, 'resize')
-            },100);
+            }, 100);
         })
     }
 
